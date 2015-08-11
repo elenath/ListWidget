6d2048566ed8761e34cda7e576174bba7be0ea78
elenath <mevtew@gmail.com>
2015-08-11 08:54:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+/////////////////////////////////// cachowanie
+
+var SingletonClass = (function () {
+
+    console.log("SDK Initialize");
+
+    function SingletonClass(element) {
+
+        var select = document.getElementById(element); // wczytanie menu rozwijanego
+
+        easyPack.getPaczkomatyList('https://api-pl.easypack24.net/v4/machines?type=0').then(function (data) {
+            //console.log("singleton start");
+            var simpleData = data._embedded.machines;
+
+            for (var i = 0; i < simpleData.length; i++) {
+                var opt = simpleData[i].address.city + " " + simpleData[i].address.street + " " + simpleData[i].address.building_no + ", " + simpleData[i].address.post_code;
+                var el = document.createElement("option");
+                el.textContent = opt;
+                el.value = opt;
+                select.appendChild(el);
+            }
+        }, function (status) { //error detection....
+            alert('Something went wrong.');
+        });
+    }
+    var instance;
+    return {
+        getInstance: function (element) {
+            if (instance == null) {
+                instance = new SingletonClass(element);
+                instance.constructor = null;
+            }
+            return instance;
+        }
+    };
+})();
+
+/////////////////////////////////// tworzenie widgetu
+
+easyPack = {};
+easyPack.ListWidget = function (element) {
+    var position = "in" + element;
+    document.getElementById('widget').innerHTML = '<select id="' + position + '"> <option>Wybierz Paczkomat</option> </select>';
+
+    console.log('Widget Initialize');
+    console.log("nazwa: " + position);
+    if (typeof element !== 'undefined') {
+        SingletonClass.getInstance(position);
+    }
+}
+
+/////////////////////////////////// funckja wczytująca plik JSON
+
+
+easyPack.getPaczkomatyList = function (url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status == 200) {
+                resolve(xhr.response);
+                console.log("file read");
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
+
+    });
+};
+
+/////////////////////////////////// uruchomienie widgetu
+
+easyPack.ListWidget('widget');
